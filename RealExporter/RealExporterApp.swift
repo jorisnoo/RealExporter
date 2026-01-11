5c4943f9f2f5cc4bdef44b2a25e1ad4d08790968
Joris Noordermeer <joris@noordermeer.ch>
2026-01-11 23:22:47 +1300

    Initial Commit

---
+++ RealExporter/RealExporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealExporterApp.swift
+//  RealExporter
+//
+//  Created by Jorge on 11.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct RealExporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
